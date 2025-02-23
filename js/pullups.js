5e094922f7b11e0bd73d72f4e7b31fb2b52662a2
Berend Swennenhuis <berendswennenhuis@gmail.com>
2025-02-23 11:27:35 +0100

    Updates

---
+++ js/pullups.js
@@ -0,0 +1,12 @@
+const fetchPullups = fetch('https://pullup-counter-backend.test.coddin.io/api/berend').then(response => {
+  return response.json();
+}).then(data => {
+  const $container = document.querySelector('[data-stronk-category="pullups"]');
+
+
+  $container.querySelector('[data-target]').dataset.target = data.count;
+
+  $container.querySelector('.pullups').innerHTML = data.count;
+
+  document.querySelector('[data-stronk-category="pullups"] [data-target]').dataset.target = data.count;
+});
